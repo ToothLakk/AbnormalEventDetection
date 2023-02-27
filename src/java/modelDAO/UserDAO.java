a1b69b74b41185bff11c984e33f9349bc51ff47d
Hoanguan <tuanp7574@gmail.com>
2023-02-27 18:20:35 +0700

    Revert "Revert "Merge branch 'master' of https://github.com/ToothLakk/AbnormalEventDetection""
    This reverts commit e2d259e9b5c26b2e764e291384a42ff5c8091b05.

--- src/java/modelDAO/UserDAO.java
+++ src/java/modelDAO/UserDAO.java
@@ -4,10 +4,30 @@
  */
 package modelDAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import model.User;
+
 /**
  *
  * @author Pham_Thai_Ha <>
  */
 public class UserDAO {
-    
+    public static User getUserByUserName(String username){
+        User user = null;
+        try (Connection conn = DBcontext.getConnection()) {
+            PreparedStatement prepstatement = conn.prepareStatement("select * from [User] where UserName=?");
+            prepstatement.setString(1, username);
+            ResultSet rs = prepstatement.executeQuery();
+            while(rs.next()){
+                user = new User(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4));
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+        return user;
+    }
 }
